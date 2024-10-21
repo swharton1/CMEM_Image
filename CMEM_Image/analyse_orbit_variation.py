Added code to create a full orbit animation to test the FOV. Discovered a bug in the tilt calculation.

CMEM_Image/analyse_orbit_variation.py
@@ -9,11 +9,11 @@ from matplotlib.patches import Rectangle
 class analyse_orbit():
     '''This will contain any plotting functions needed'''
     
-    def __init__(self, fname='parameter_output.pkl', orbit_folder_name='ra2_rp19_inc70_raan180_omega300/', ppmlr_folder='S05D20V400B0000-05rad.dat/'):
+    def __init__(self, fname='orbital_limb_output_corrected.pkl', orbit_folder_name='20260402-20260403/', ppmlr_folder='S05D20V400B0000-05rad.fits/'):
         '''Sorts out paths and reads in the file.'''
         
         self.fname = fname 
-        self.plot_path = os.environ.get('PLOT_PATH')+'fitted_orbit_sim/'+orbit_folder_name+ppmlr_folder 
+        self.plot_path = os.environ.get('PLOT_PATH')+'fitted_orbit_sim_limb/'+orbit_folder_name+ppmlr_folder 
         self.pickle_path = os.environ.get('PICKLE_PATH')
         
         self.target_dict = self.read_pickle(self.plot_path+self.fname)
@@ -25,7 +25,7 @@ class analyse_orbit():
             pickle_dict = pickle.load(f)
         return pickle_dict
     
-    def plot_orbit(self, add_cost=False):
+    def plot_orbit(self, add_cost=False, figname='Orbit_1_analysis.png', aim_ylim=[4,12]):
         '''This will plot a graph of how the subsolar magnetopause radii 
         determined by CMEM and extracted from the PPMLR simulation vary
         with the number of pixels. This will show us the systematic error.'''
@@ -33,34 +33,34 @@ class analyse_orbit():
         #Extract key values. 
         maxIx = self.target_dict['maxIx']
         maxdIx = self.target_dict['maxdIx']
-        f25 = self.target_dict['f.25'][0] 
+        f25 = self.target_dict['f.25']
         n_pixels = np.array(self.target_dict['n_pixels'])
         m_pixels = np.array(self.target_dict['m_pixels']) 
         cmem_mp = np.array(self.target_dict['cmem_mp']) 
         inout = np.array(self.target_dict['inout'])
         smile_loc = np.array(self.target_dict['smile_loc']) 
         density = self.target_dict['density'] 
-        time = self.target_dict['t_list'] 
-        xgsm = self.target_dict['x_gsm']
-        ygsm = self.target_dict['y_gsm']
-        zgsm = self.target_dict['z_gsm'] 
-        target_loc = self.target_dict['target_loc']
+        time = np.array(self.target_dict['t_list'])
+        xgsm = np.array(self.target_dict['x_gsm'])
+        ygsm = np.array(self.target_dict['y_gsm'])
+        zgsm = np.array(self.target_dict['z_gsm']) 
+        target_loc = np.array(self.target_dict['target_loc']).T[0]
         min_cost = np.array(self.target_dict['min_cost'])
+        tilt = np.array(self.target_dict['tilt']) 
         
+        #Get time axis in hours. 
+        delta_time = np.arange(time.size) 
         
         #Select data where s/c is inside MP and where it is outside MP. 
         inside = np.where(inout == 'in')
         outside = np.where(inout == 'out')
         
         #Get time resolution. 
-        time_res = time[1] - time[0] 
-        
+        #time_res = time[1] - time[0] 
+        time_res=1
         #Identify midpoint time for exit and entry to magnetosphere. 
-        intime = time[inside]
-        outtime = time[outside] 
-        
-        exit_time = outtime[0] - time_res/2. 
-        entry_time = outtime[-1] + time_res/2. 
+        intime = delta_time[inside]
+        outtime = delta_time[outside] 
         
         #Make the figure. 
         if add_cost:
@@ -79,77 +79,104 @@ class analyse_orbit():
         else:
             fig = plt.figure(figsize=(6,6))
             fig.subplots_adjust(bottom=0.10)
-            ax = fig.add_subplot(211) 
-            ax2 = fig.add_subplot(212)
-            
+            ax = fig.add_subplot(311) 
+            ax2 = fig.add_subplot(312)
+            ax3 = fig.add_subplot(313)
+                        
             #Add labels. 
-            ax2.set_xlabel('Time Since Periapsis (hours)') 
-            ax.set_ylabel('Subsolar Magnetopause Position [RE]')
-            ax2.set_ylabel('Spacecraft Position [RE]') 
+            ax3.set_xlabel('Time (hours)') 
+            ax.set_ylabel('Subsolar Magnetopause\n Position [RE]')
+            ax2.set_ylabel('Spacecraft \nPosition [RE]') 
+            ax3.set_ylabel('Aim Point [RE]') 
+            #ax3b = ax3.twinx()
+            #ax3b.set_ylabel('Tilt Angle [deg]', color='b') 
             
-        
-        
-        #Get array for total number of pixels. 
-        #self.total_pixels = n_pixels*m_pixels 
         
         #Add on the PPMLR values. 
         #xlims = [self.total_pixels[0], self.total_pixels[-1]] 
-        xlims = [time[0], time[-1]]
+        xlims = [delta_time[0], delta_time[-1]]
         ax.plot(xlims, [maxIx, maxIx], 'b-', label='maxIx')
         ax.plot(xlims, [f25, f25], 'g-', label='f.25')  
         ax.plot(xlims, [maxdIx, maxdIx], 'r-', label='maxdIx')
         
         #Add on the CMEM determinations. 
-        ax.plot(time, cmem_mp, 'k-', label='CMEM', marker='x') 
+        ax.plot(delta_time, cmem_mp, 'k-', label='CMEM', marker='x') 
         
-        #ax.set_xticks(m_pixels) 
-        #xticks = ["{}\n{}".format(m_pixels[x], self.total_pixels[x]) for x in range(len(m_pixels))]
-        #ax.set_xticklabels(xticks)
-        #ax.set_xlim(0,55)
-        
-        
-        ax.set_title('Target = ({:.2f},{:.2f},{:.2f}), n = {:.2f} cm'.format(target_loc[0], target_loc[1], target_loc[2], density)+r'$^{-3}$')
+
         
         #Add orbit information onto plot below. 
         r = np.sqrt(xgsm**2 + ygsm**2 + zgsm**2) 
-        ax2.plot(time, xgsm, 'b-', label='x', marker='x')
-        ax2.plot(time, ygsm, 'r-', label='y', marker='x')
-        ax2.plot(time, zgsm, 'g-', label='z', marker='x') 
-        ax2.plot(time, r, 'k-', label='r', marker='x')
+        ax2.plot(delta_time, xgsm, 'b-', label='x', marker='x')
+        ax2.plot(delta_time, ygsm, 'r-', label='y', marker='x')
+        ax2.plot(delta_time, zgsm, 'g-', label='z', marker='x') 
+        ax2.plot(delta_time, r, 'k-', label='r', marker='x')
+        
+        #Add on aim point and tilt angle. 
+        ax3.plot(delta_time, target_loc, 'k-', marker='x') 
+        #ax3b.plot(time, np.rad2deg(tilt), 'b-', marker='x') 
+        
         
         if add_cost:
             #Add the cost values on. 
             ax3.plot(time, min_cost, 'k') 
         
+        #Loop through the intime array to show when SMILE is thought to be inside the magnetopause. 
+        for i, ival in enumerate(intime):
+            
+            #Add patch to top figure. 
+            rect1 = Rectangle([ival,0], height=15, width=time_res, color='gray', alpha=0.4)
+            ax.add_patch(rect1)
+            
+            #Add patch to second figure. 
+            rect2 = Rectangle([ival, -20], height=50, width=time_res, color='gray', alpha=0.4)
+            ax2.add_patch(rect2) 
+            
+            #Add patch to third figure. 
+            rect3 = Rectangle([ival, 0], height=50, width=time_res, color='gray', alpha=0.4)
+            ax3.add_patch(rect3) 
         
-        #Add rectangles in grey to show areas inside the magnetopause. 
-        rect1 = Rectangle([time[0], 0], height=15, width=exit_time, color='gray', alpha=0.4)
-        rect2 = Rectangle([entry_time, 0], height=15, width=time[-1]-entry_time, color='gray', alpha=0.4)
-        rect3 = Rectangle([time[0], -20], height=50, width=exit_time, color='gray', alpha=0.4)
-        rect4 = Rectangle([entry_time, -20], height=50, width=time[-1]-entry_time, color='gray', alpha=0.4)
-        if add_cost:
-            rect5 = Rectangle([time[0], 0], height=10, width=exit_time, color='gray', alpha=0.4)
-            rect6 = Rectangle([entry_time, 0], height=10, width=time[-1]-entry_time, color='gray', alpha=0.4)
         
-        ax.add_patch(rect1)
-        ax.add_patch(rect2)
-        ax2.add_patch(rect3)
-        ax2.add_patch(rect4) 
-        if add_cost:
-            ax3.add_patch(rect5)
-            ax3.add_patch(rect6) 
+        #if add_cost:
+        #    rect5 = Rectangle([time[0], 0], height=10, width=exit_time, color='gray', alpha=0.4)
+        #    rect6 = Rectangle([entry_time, 0], height=10, width=time[-1]-entry_time, color='gray', alpha=0.4, linecolor=None)
+        
+       
+        #if add_cost:
+        #    ax3.add_patch(rect5)
+        #    ax3.add_patch(rect6) 
         
         #Reset your limits. 
         ax.set_xlim(xlims) 
         ax.set_ylim(6.5,10.5)
-        ax.legend(loc='best', fontsize=8)
-        ax.grid()
+        #ax.legend(loc='best', fontsize=8)
+        ax.grid() 
+        
+        #Add labels to side. 
+        ax.text(1.01, 0.99, 'max Ix', color='b', ha='left', va='top', transform=ax.transAxes, fontsize=8)
+        ax.text(1.01, 0.89, 'f.25', color='g', ha='left', va='top', transform=ax.transAxes, fontsize=8)
+        ax.text(1.01, 0.79, 'max dIx', color='r', ha='left', va='top', transform=ax.transAxes, fontsize=8)
+        ax.text(1.01, 0.69, 'CMEM', color='k', ha='left', va='top', transform=ax.transAxes, fontsize=8)
         
         #Reset your limits. 
         ax2.set_xlim(xlims) 
         ax2.set_ylim(-20,30)
-        ax2.legend(loc='best', fontsize=8)
+        #ax2.legend(loc='best', fontsize=8)
         ax2.grid()
+        
+        #Add labels to side. 
+        ax2.text(1.01, 0.99, 'x', color='b', ha='left', va='top', transform=ax2.transAxes, fontsize=8)
+        ax2.text(1.01, 0.89, 'y', color='r', ha='left', va='top', transform=ax2.transAxes, fontsize=8)
+        ax2.text(1.01, 0.79, 'z', color='g', ha='left', va='top', transform=ax2.transAxes, fontsize=8)
+        ax2.text(1.01, 0.69, 'r', color='k', ha='left', va='top', transform=ax2.transAxes, fontsize=8)
+        
+        ax3.set_xlim(xlims)
+        ax3.set_ylim(aim_ylim)
+        ax3.grid()
+        
+        #Add a title. 
+        start = time[0]
+        end = time[-1]
+        fig.text(0.5, 0.9, start.strftime("%Y%m%d %H:%M")+' - '+start.strftime("%Y%m%d %H:%M"), ha='center')
         
         if add_cost:
             #Reset your limits. 
@@ -162,5 +189,5 @@ class analyse_orbit():
             fig.savefig(self.plot_path+'orbit_variation_analysis_with_cost.png')
         else:
             #Save the plot. 
-            fig.savefig(self.plot_path+'orbit_variation_analysis.png') 
+            fig.savefig(self.plot_path+figname) 
         

CMEM_Image/smile_fov_limb.py
@@ -10,7 +10,7 @@ class smile_limb():
     '''This object will use the spacecraft position and limb angle to work out the pointing and 
     target directions, along with everything else.''' 
     
-    def __init__(self, theta_fov=27, phi_fov=16, n_pixels=4, m_pixels=2, smile_loc=(0,-10,10), p_spacing=0.5, p_max=80, limb=20.3):
+    def __init__(self, theta_fov=27, phi_fov=16, n_pixels=4, m_pixels=2, smile_loc=(0,-10,10), p_spacing=0.5, p_max=None, limb=20.3):
         '''This takes in all the initial parameters 
         
         Parameters
@@ -21,7 +21,7 @@ class smile_limb():
         m_pixels - Number of pixels in the phi direction (camera coords)
         smile_loc - vector for the position of smile in magnetospheric xyz coords. 
         p_spacing - space in RE along LOS between points at which to calculate. 
-        p_max - maximum distance in RE from spacecraft it will integrate to. 
+        p_max - maximum distance in RE from spacecraft it will integrate to. If it is set to None, it will be made equal to the length of vector L, the look vector to the aim point.  
         
         '''
         
@@ -63,14 +63,6 @@ class smile_limb():
         self.y_unit = np.array([0,1,0])
         self.z_unit = np.array([0,0,1])
         
-        #Get nhat, the unit vector perpendicular to the vertical meridian containing the look vector. 
-        self.n = np.cross(self.z_unit, self.L) 
-        self.n_unit = self.n/np.sqrt(self.n[0]**2 + self.n[1]**2 + self.n[2]**2) 
-        
-        #Get the SXI tilt angle. 
-        cos_tilt = np.dot(-self.b_unit, self.n_unit)
-        self.sxi_tilt = -np.arccos(cos_tilt) 
-        
         #Get the colatitude of the look direction. 
         cos_colat = self.L[2]/self.Lmag 
         self.sxi_theta = np.arccos(cos_colat) 
@@ -78,42 +70,84 @@ class smile_limb():
         #Get the longitude of the look direction. 
         self.sxi_phi = np.arctan2(self.L[1], self.L[0])
         
-        print ('Tilt = ', np.rad2deg(self.sxi_tilt))
-        print ('Colat = ', np.rad2deg(self.sxi_theta))
-        print ('Long. = ', np.rad2deg(self.sxi_phi)) 
+        #NEW METHOD. 
+        #ROTATE Y UNIT VECTOR AROUND Z AXIS. NO POINT ROTATING AROUND Y AXIS!  
+        self.yhat_rot = np.array([-np.sin(self.sxi_phi), np.cos(self.sxi_phi),0]) 
         
-        #Set LoS calculations to only got to the target point. 
-        self.p_max = self.Lmag
+        #ROTATE Z UNIT VECTOR AROUND Y AXIS AND Z AXIS LIKE DATA! 
+        # Calculate the rotation angle a from theta. a is the increase in colatitude. 
+        a = -(np.pi/2. - self.sxi_theta)
+        self.zhat_rot1 = np.array([np.sin(a),0,np.cos(a)])
+        self.zhat_rot2 = np.array([self.zhat_rot1[0]*np.cos(self.sxi_phi),self.zhat_rot1[0]*np.sin(self.sxi_phi),self.zhat_rot1[2]])
+        
+        #Calculate the cross product of b and L, then get the unit vector. 
+        yimage = np.cross(self.b, self.L) 
+        yimage_mag = np.sqrt(yimage[0]**2 + yimage[1]**2 + yimage[2]**2)
+        self.yimage_unit = yimage/yimage_mag 
+        
+        #Do some fundamental tests to ensure no calculation errors have taken place up to here. 
+        try: 
+            #The cross product of these should be 0 to 5 dp. 
+            np.testing.assert_array_almost_equal(np.cross(self.yhat_rot, self.zhat_rot2), self.L_unit, decimal=5) 
+            assert(np.dot(self.yimage_unit, self.b) == 0) 
+            assert(round(np.dot(self.yhat_rot, self.zhat_rot2),10) == 0) 
+        except AssertionError:
+            print ('Error in rotation of y and z unit vectors.')
+        
+        
+        
+        cos_tilt2 = np.dot(self.b_unit, self.yhat_rot)
+        self.sxi_tilt = np.arccos(cos_tilt2) 
+        
+        #OLD METHOD. 
+        #Get nhat, the unit vector perpendicular to the vertical meridian containing the look vector. 
+        self.n = np.cross(self.z_unit, self.L) 
+        self.n_unit = self.n/np.sqrt(self.n[0]**2 + self.n[1]**2 + self.n[2]**2) 
+        
+        #Get the SXI tilt angle. 
+        cos_tilt = np.dot(-self.b_unit, self.n_unit)
+        self.sxi_tilt2 = -np.arccos(cos_tilt) 
+        
+        
+        
+        #print ('Tilt = ', np.rad2deg(self.sxi_tilt))
+        #print ('Colat = ', np.rad2deg(self.sxi_theta))
+        #print ('Long. = ', np.rad2deg(self.sxi_phi)) 
+        
+        #Set LoS calculations to only got to the target point if p_max is None. 
+        if self.p_max is None:
+            self.p_max = self.Lmag
+        
         
         ts = process_time()
-        print ("Get theta and phi for each pixel:")
+        #print ("Get theta and phi for each pixel:")
         self.get_theta_and_phi_all_pixels()
         te = process_time()
-        print ("Time = {:.1f}s".format(te-ts))
+        #print ("Time = {:.1f}s".format(te-ts))
 
         ts = process_time()
-        print ("Get vector for each pixel:")
+        #print ("Get vector for each pixel:")
         self.get_vector_for_all_pixels()
         te = process_time()
-        print ("Time = {:.1f}s".format(te-ts))
+        #print ("Time = {:.1f}s".format(te-ts))
 
         ts = process_time()
-        print ("Tilt camera: ")
+        #print ("Tilt camera: ")
         self.tilt_sxi_camera() 
         te = process_time()
-        print ("Time = {:.1f}s".format(te-ts))
+        #print ("Time = {:.1f}s".format(te-ts))
 
         ts = process_time()
-        print ("Rotate camera: ")
+        #print ("Rotate camera: ")
         self.rotate_camera()
         te = process_time()
-        print ("Time = {:.1f}s".format(te-ts))
+        #print ("Time = {:.1f}s".format(te-ts))
 
         ts = process_time()
-        print ("Get LOS coordinates: ")
+        #print ("Get LOS coordinates: ")
         self.get_LOS_coords()
         te = process_time()
-        print ("Time = {:.1f}s".format(te-ts))
+        #print ("Time = {:.1f}s".format(te-ts))
         
     def get_alpha_angle(self):
         '''This will calculate alpha, the angle between the spacecraft vector and the perpendicular to the x axis. '''
@@ -144,12 +178,12 @@ class smile_limb():
 
     def plot_vectors(self, elev=45, azim=45):
         '''This will plot all the vectors to make sense of them.'''
-        
+        #plt.close("all")
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d') 
         
         #Add SMILE vector. 
-        ax.plot([0, self.smile_loc[0]], [0, self.smile_loc[1]], [0, self.smile_loc[2]], 'k-', label='SMILE') 
+        ax.plot([0, self.smile_loc[0]], [0, self.smile_loc[1]], [0, self.smile_loc[2]], 'b-', label='SMILE') 
         
         #Add Look vector. 
         ax.plot([self.smile_loc[0], self.smile_loc[0]+self.L[0]], [self.smile_loc[1], self.smile_loc[1]+self.L[1]], [self.smile_loc[2], self.smile_loc[2]+self.L[2]], 'r-', label='Look')
@@ -161,7 +195,25 @@ class smile_limb():
         ax.plot([0, self.b[0]], [0, self.b[1]], [0, self.b[2]], 'c-', label='b') 
         
         #Add vector n unit. 
-        ax.plot([self.b[0], self.b[0]+self.n_unit[0]], [self.b[1], self.b[1]+self.n_unit[1]], [self.b[2], self.b[2]+self.n_unit[2]], 'm-', label='n_unit')
+        #ax.plot([self.b[0], self.b[0]+self.n_unit[0]], [self.b[1], self.b[1]+self.n_unit[1]], [self.b[2], self.b[2]+self.n_unit[2]], 'm-', label='n_unit')
+        
+        
+        #Add rotated y vector. These have not had a tilt applied.  
+        ax.plot([self.smile_loc[0], self.smile_loc[0]+self.yhat_rot[0]], [self.smile_loc[1], self.smile_loc[1]+self.yhat_rot[1]], [self.smile_loc[2], self.smile_loc[2]+self.yhat_rot[2]], color='orange', linestyle='-', label='y_rot')
+        
+        #Add rotated z vector. These have not had a tilt applied. 
+        ax.plot([self.smile_loc[0], self.smile_loc[0]+self.zhat_rot2[0]], [self.smile_loc[1], self.smile_loc[1]+self.zhat_rot2[1]], [self.smile_loc[2], self.smile_loc[2]+self.zhat_rot2[2]], color='k', linestyle='-', label='z_rot')
+        
+        #Add rotated y vector. These have not had a tilt applied.  
+        ax.plot([self.b[0], self.b[0]+self.yhat_rot[0]], [self.b[1], self.b[1]+self.yhat_rot[1]], [self.b[2], self.b[2]+self.yhat_rot[2]], color='orange', linestyle='-')
+        
+        #Add rotated z vector. These have not had a tilt applied. 
+        ax.plot([self.b[0], self.b[0]+self.zhat_rot2[0]], [self.b[1], self.b[1]+self.zhat_rot2[1]], [self.b[2], self.b[2]+self.zhat_rot2[2]], color='k', linestyle='-')
+        
+        #Add the yimage unit vector. 
+        ax.plot([self.b[0], self.b[0]+self.yimage_unit[0]], [self.b[1], self.b[1]+self.yimage_unit[1]], [self.b[2], self.b[2]+self.yimage_unit[2]], color='c', linestyle='-')
+        
+        self.add_earth(ax)
         
         #Sort legend and labels. 
         ax.legend(loc='best')